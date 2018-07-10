43bb17c69186c17a495870c1ff2bdb4dcb75c7be
DetectiveAzul <detec.azul@gmail.com>
2018-07-10 19:42:55 +0100

    Refactored seed with the actual structure for every item of the API

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,45 +1,10 @@
 use recetarium;
 db.dropDatabase();
 
-db.recipes.insertMany([
-  {
-  playerName: "Andris",
-  totalAnswers: 40,
-  totalCorrectAnswer: 2,
-  totalGames: 2,
-  totalWinGames: 0,
-  winRatio: 0.00,
-  totalCorrectAnswersRatio: 5.00,
-  averageCorrectAnswers: 1
-},
-  {
-  playerName: "Jemma",
-  totalAnswers: 800,
-  totalCorrectAnswer: 540,
-  totalGames: 80,
-  totalWinGames: 54,
-  winRatio: 67.50,
-  totalCorrectAnswersRatio: 67.50,
-  averageCorrectAnswers: 6.75
-},
-  {
-  playerName: "Alex S",
-  totalAnswers: 600,
-  totalCorrectAnswer: 200,
-  totalGames: 1,
-  totalWinGames: 0,
-  winRatio: 0.00,
-  totalCorrectAnswersRatio: 33.33,
-  averageCorrectAnswers: 200
-},
-  {
-  playerName: "Jaime",
-  totalAnswers: 10,
-  totalCorrectAnswer: 2,
-  totalGames: 1,
-  totalWinGames: 0,
-  winRatio: 0.00,
-  totalCorrectAnswersRatio: 20.00,
-  averageCorrectAnswers: 2
-}]
-);
+db.recipes.insert({
+  name: 'Butternut Squash Pie',
+  tags: ['vegan', 'desserts'],
+  ingredients: ['butternut', 'pie'],
+  preparation: ['Squash the butternut', 'put it into a pie'],
+  image: ['https://www.thespruceeats.com/thmb/RjD5meb6CcQGLfn2OptQw7OPsg0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/butternut-squash-pie-15-56a8bab85f9b58b7d0f4a523.jpg']
+});
