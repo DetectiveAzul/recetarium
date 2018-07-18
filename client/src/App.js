8f1162320e302f40ea1c767964234beee63e343b
Cian <detec.azul@gmail.com>
2018-07-18 18:02:35 +0100

    Testing API integration with the Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import Request from './helpers/request.js';
 
 class App extends Component {
   render() {
+    //Test connection with API is working
+    // const request = new Request('http://localhost:8080/api/recipes');
+    // request.get()
+    //   .then((recipes) => {
+    //     console.log(recipes)
+    //   });
+
+    //Returning the content from the API
     return (
-      <h1>Hello World</h1>
+      <div className='content'>
+      </div>
     );
   }
 }
