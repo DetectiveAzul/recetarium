d8c5a446fc9a3bcc8adb35b1fe15c953e7b7bcd3
Cian <detec.azul@gmail.com>
2018-07-18 17:07:50 +0100

    Added port redirecting on a variable for heroku deploy

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const path = require('path');
 const parser = require('body-parser');
 const indexRouter = require('./routers/index_router.js');
 
+//For heroku
+let port = process.env.PORT || 8080;
+
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
 app.use(parser.json());
 app.use(indexRouter);
 
-app.listen(3001, function () {
+app.listen(port, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });
