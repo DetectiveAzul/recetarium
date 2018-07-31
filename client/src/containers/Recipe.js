bf076702addb269729ce153d65889d98397728bf
Jaime Lopez <detec.azul@gmail.com>
2018-07-31 16:57:10 +0100

    Temporary form can find the recipe inputing the index on the text field. Form prevent event default

--- client/src/containers/Recipe.js
+++ client/src/containers/Recipe.js
@@ -4,14 +4,27 @@ class Recipe extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentRecipe: 0
+      currentRecipe: 0,
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   };
 
-  render() {
+  handleSubmit(event) {
+    event.preventDefault();
+    const index = event.target.recipeIndex.value
+    console.log('Submitted!', index);
+    console.log('Recipe selected:', this.props.recipes[index]);
+  };
 
+
+  render() {
     return(
       <div className='Recipe'>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' name='recipeIndex' />
+          <input type='submit' value='Search' />
+        </form>
       </div>
     );
   };
