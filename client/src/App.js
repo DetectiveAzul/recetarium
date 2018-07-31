1bcbe37858bca2c6b231d3d347411983c41a9df1
8403464 <a8403464@8403464s-MacBook-Pro.local>
2018-07-31 14:21:06 +0100

    Removed temporary code to test API integration

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,14 @@ import RequestTest from './containers/RequestTest.js';
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      recipes: []
     }
   }
 
-  componentDidMount() {
-    const url = 'http://recetarium-api.herokuapp.com/api/recipes';
-    fetch(url)
-    .then(res =>  res.json() )
-    .then(recipesData => this.setState({recipes: recipesData }))
-    .catch(error => console.log('Error:', error));
-  };
 
   render() {
     return (
       <div className='App'>
-        <RequestTest recipes= {this.state.recipes} />
+
       </div>
     );
   }
