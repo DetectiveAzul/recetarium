dc70da8aa15f59b8d8cbb52ee68df7fab8da6497
Jaime Lopez <detec.azul@gmail.com>
2018-07-31 15:36:49 +0100

    Created Recipe class react container. Moved API fetch to a helper js file called Request. Removed Test containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import RequestTest from './containers/RequestTest.js';
+import Recipe from './containers/Recipe.js';
+import Request from './helpers/Request.js';
 
 class App extends Component {
-  constructor() {
-    super()
-    }
-  }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
 
+  componentDidMount() {
+    const request = new Request('http://recetarium-api.herokuapp.com/api/recipes');
+    request.get()
+    .then(recipesData => this.setState({recipes: recipesData }) )
+  };
 
   render() {
     return (
       <div className='App'>
-
+        <Recipe recipes = {this.state.recipes} />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
---
+++ client/src/containers/Recipe.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+  
+    };
+  };
+
+  render() {
+
+    return(
+      <div className='Recipe'>
+      </div>
+    );
+  };
+
+
+
+}
+
+export default Recipe;
